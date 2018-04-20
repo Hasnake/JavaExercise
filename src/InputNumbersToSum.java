ecdfcc65ffb95944ed0047707da44c392f4bf9af
Haile Asnake <hasnake@sbgtv.com>
2018-04-20 10:50:50 -0700

    Adding the scanner

---
+++ src/InputNumbersToSum.java
@@ -0,0 +1,30 @@
+/**
+ * Created by hasnake on 4/20/18.
+ */
+import java.util.Scanner;
+public class InputNumbersToSum{
+
+
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner (System.in);
+
+        //First number
+        System.out.print("Input the first number: ");
+        int num1 = input.nextInt();
+
+        //second number
+        System.out.print("Input the second number: ");
+        int num2 = input.nextInt();
+        //third number
+        System.out.print("Input the third number: ");
+        int num3 = input.nextInt();
+
+        //Sum
+        int sum = num1 + num2+num3;
+        System.out.println();
+        System.out.println("Sum: "+sum);
+    }
+
+}
---
+++ src/SumOfTwoNumbers.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hasnake on 4/20/18.
+ */
+public class SumOfTwoNumbers {
+    public static void main(String[] args){
+        System.out.println(23+45);
+    }
+}
