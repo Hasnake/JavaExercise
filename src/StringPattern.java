ac77f9989e30666e27000b80b5f3c11cbce57ca2
Haile Asnake <hasnake@sbgtv.com>
2018-04-27 14:26:46 -0700

    String Friday

---
+++ src/StringPattern.java
@@ -0,0 +1,16 @@
+/**
+ * Created by hasnake on 4/27/18.
+ */
+public class StringPattern {
+    public static void main(String[] args) {
+
+        System.out.println("   FF  FFF      RRRRRRRR        II       DD DD DD                           aa                YYY          YY     ");
+        System.out.println("   FF           RR     RR       II       DD       DD                     aa    aa               YYY       YYY    ");
+        System.out.println("   FF  FF       RR     RR       II       DD        DD                   aa       aa              YYY    YYYY     ");
+        System.out.println("   FF           RRRRRR          II       DD          DD               aaaaaaaaaaaaaaa                YYY          ");
+        System.out.println("   FF           RR     RR       II       DD          DD              aaaaaaaaaaaaaaaaaa              YYY          ");
+        System.out.println("   FF           RR      RR      II       DD         DD             aa                  aa            YYY          ");
+        System.out.println("   FF           RR        RR    II       DD DD DD DD              aa                    aa           YYY          ");
+    }
+}
+
