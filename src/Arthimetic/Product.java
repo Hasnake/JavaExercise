d5c1359954063f296792713da30c914732644a1b
Haile Asnake <hasnake@sbgtv.com>
2018-04-27 12:53:59 -0700

    Adding Product

--- src/Arthimetic/Product.java
+++ src/Arthimetic/Product.java
@@ -1,20 +1,22 @@
 package Arthimetic;
+
 import java.util.Scanner;
 
 /**
  * Created by hasnake on 4/27/18.
  */
-public class Division {
+public class Product {
     public static void main(String[] args)
     {
         Scanner input = new Scanner (System.in);//
 
         System.out.print("Input the first number: ");
-        double a = input.nextDouble();//parsing the input text to the desired data type.
+        int a = input.nextInt();//parsing the input text to the desired data type.
         System.out.print("Input the second number: ");
-        double b = input.nextDouble();
-        double d = (a/b);
-        System.out.println();
-        System.out.println("The division of a and b is:" +d);
+        int b = input.nextInt();
+        int d = (a*b);
+        System.out.println("Expected output");
+        System.out.println(a + "*" + b+ " = " +d);
     }
 }
+
